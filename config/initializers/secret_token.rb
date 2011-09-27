16d3e9b329c50695ea33bf9fbd24585f67f16c59
Joshua Newman <j@orson.com>
2011-09-27 00:25:48 -0400

    Initiail commit 2.0.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenlight::Application.config.secret_token = '25872907a2c87081b82fb19a5a896f1d4873c57b8e199a7d8138679497e9f1c962f18e657123e167fe3e410d4215e0076bb4c46f5a2cdadb3fb618043170c652'
